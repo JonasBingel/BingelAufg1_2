f0c84b02f76bb1a2054773d8b3524a03a847f498
JonasBingel <30416267+JonasBingel@users.noreply.github.com>
2018-10-15 21:20:35 +0200

    removed redundancies
    all switch statements that are needed to calculate the amount of days in
    month were reduced into one

--- src/pack/Termine.java
+++ src/pack/Termine.java
@@ -1,92 +1,47 @@
 package pack;
 
 public class Termine {
-
-	public static void main(String[] args) {
-		final int TAG = 22;
-		final int MONAT = 4;
-		final int JAHR = 2019;
-		final int BEARBEITUNGSZEIT = 8;
-		final int BEARBEITUNGSZEIT_TAGE = BEARBEITUNGSZEIT * 7;
-
-		String wochentag = berechneWochentag(TAG, MONAT, JAHR);
-		String datumAusgang = formatiereDatum(TAG, MONAT, JAHR);
-		ausgebenAusgangsdatum(wochentag, datumAusgang, BEARBEITUNGSZEIT);
-
-		if (pruefeDatum(TAG, MONAT, JAHR)) {
-
-			if (!wochentag.equals("Samstag") && !wochentag.equals("Sonntag")) {
-
-				String datumv = berechneVorwaertsDatum(TAG, MONAT, JAHR, BEARBEITUNGSZEIT_TAGE);
-				String datumr = berechnerueckwaertsDatum(TAG, MONAT, JAHR, BEARBEITUNGSZEIT_TAGE);
-
-				ausgebenTerminierung(wochentag, datumv, "vorwaerts");
-				ausgebenTerminierung(wochentag, datumr, "rueckwaerts");
-			} else {
-				System.out.println("Ausgangsdatum ist kein Wochentag");
-			}
-		} else {
-			System.out.println("Geben Sie ein gueltiges Datum an");
-		}
-	}
+	// TODO Kommentare hinzufuegen && Conventions pruefen
 
 	public static boolean pruefeDatum(int tag, int monat, int jahr) {
 
 		if (1582 <= jahr && jahr <= 3000) {
-			switch (monat) {
-			case 1:
-			case 3:
-			case 5:
-			case 7:
-			case 8:
-			case 10:
-			case 12:
-				if (0 < tag && tag <= 31) {
-					return true;
-				} else {
-					return false;
-				}
-			case 4:
-			case 6:
-			case 9:
-			case 11:
-				if (0 < tag && tag <= 30) {
-					return true;
-				}
-			case 2:
-				if (pruefeSchaltjahr(jahr) == true && 0 < tag && tag <= 29) {
-					return true;
-				} else if (pruefeSchaltjahr(jahr) == false && 0 < tag && tag <= 28) {
-					return true;
-				} else {
-					return false;
-				}
-			default:
+			if (0 < tag && tag <= berechneAnzahlTageinMonat(monat, jahr)) {
+				return true;
+			} else {
 				return false;
 			}
-
 		}
 		return false;
 	}
-
-	public static String formatiereDatum(int tag, int monat, int jahr) {
-		String tagFormatiert;
-		String monatFormatiert;
-		if (tag < 10) {
-			tagFormatiert = "0" + tag;
-		} else {
-			tagFormatiert = Integer.toString(tag);
+	
+	public static int berechneAnzahlTageinMonat(int monat, int jahr) {
+		switch (monat) {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			return 31;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			return 30;
+		// case 2 is default
+		case 2:
+			if (pruefeSchaltjahr(jahr) == true) {
+				return 29;
+			} else {
+				return 28;
+			}
+		default:
+			return 0;
 		}
-		if (monat < 10) {
-			monatFormatiert = "0" + monat;
-		} else {
-			monatFormatiert = Integer.toString(monat);
-		}
-
-		return (tagFormatiert + "." + monatFormatiert + "." + jahr);
-
 	}
-
+	
 	public static String berechneWochentag(int tag, int monat, int jahr) {
 
 		int h;
@@ -136,6 +91,23 @@ public class Termine {
 			return false;
 		}
 	}
+	
+	public static String formatiereDatum(int tag, int monat, int jahr) {
+		String tagFormatiert;
+		String monatFormatiert;
+		if (tag < 10) {
+			tagFormatiert = "0" + tag;
+		} else {
+			tagFormatiert = Integer.toString(tag);
+		}
+		if (monat < 10) {
+			monatFormatiert = "0" + monat;
+		} else {
+			monatFormatiert = Integer.toString(monat);
+		}
+
+		return (tagFormatiert + "." + monatFormatiert + "." + jahr);
+	}
 
 	public static String berechneVorwaertsDatum(int tag, int monat, int jahr, int anzahlTage) {
 		anzahlTage = anzahlTage + tag - 1;
@@ -144,85 +116,31 @@ public class Termine {
 		while (anzahlTage > 0) {
 			int anzahlTageMonat;
 
-			switch (monat) {
-			case 1:
-			case 3:
-			case 5:
-			case 7:
-			case 8:
-			case 10:
-				anzahlTageMonat = 31;
+			if (monat != 12) {
+				anzahlTageMonat = berechneAnzahlTageinMonat(monat, jahr);
 				if (anzahlTage >= anzahlTageMonat) {
 					anzahlTage = anzahlTage - anzahlTageMonat;
 					monat = monat + 1;
-					break;
 				} else {
 					tag = tag + anzahlTage;
 					anzahlTage = 0;
 					break;
 				}
-
-			case 4:
-			case 6:
-			case 9:
-			case 11:
-				anzahlTageMonat = 30;
-				if (anzahlTage >= anzahlTageMonat) {
-					anzahlTage = anzahlTage - anzahlTageMonat;
-					monat = monat + 1;
-					break;
-				} else {
-					tag = tag + anzahlTage;
-					anzahlTage = 0;
-					break;
-				}
-
-			case 2:
-				if (pruefeSchaltjahr(jahr)) {
-					anzahlTageMonat = 29;
-					if (anzahlTage >= anzahlTageMonat) {
-						anzahlTage = anzahlTage - anzahlTageMonat;
-						monat = monat + 1;
-						break;
-					} else {
-						tag = tag + anzahlTage;
-						anzahlTage = 0;
-						break;
-					}
-
-				} else {
-					anzahlTageMonat = 28;
-					if (anzahlTage >= anzahlTageMonat) {
-						anzahlTage = anzahlTage - anzahlTageMonat;
-						monat = monat + 1;
-						break;
-					} else {
-						tag = tag + anzahlTage;
-						anzahlTage = 0;
-						break;
-					}
-
-				}
-
-			default:
-				anzahlTageMonat = 31;
+			} else {
+				anzahlTageMonat = berechneAnzahlTageinMonat(monat, jahr);
 				if (anzahlTage >= anzahlTageMonat) {
 					anzahlTage = anzahlTage - anzahlTageMonat;
 					monat = 1;
 					jahr = jahr + 1;
-					break;
 				} else {
 					tag = tag + anzahlTage;
 					anzahlTage = 0;
-					break;
 				}
-
 			}
 
 		}
-
 		return formatiereDatum(tag, monat, jahr);
-	}
+	}	
 
 	public static String berechnerueckwaertsDatum(int tag, int monat, int jahr, int anzahlTage) {
 		anzahlTage = anzahlTage - tag;
@@ -230,89 +148,34 @@ public class Termine {
 		while (anzahlTage > 0) {
 			int anzahlTageMonat;
 
-			switch (monat) {
-			case 2:
-			case 8:
-			case 4:
-			case 6:
-			case 9:
-			case 11:
-				anzahlTageMonat = 31;
+			
+			if (monat != 1) {
+				// es müssen die Monate des Vormonats genommen werden
+				anzahlTageMonat = berechneAnzahlTageinMonat((monat - 1), jahr);
 				if (anzahlTage >= anzahlTageMonat) {
 					anzahlTage = anzahlTage - anzahlTageMonat;
 					monat = monat - 1;
-					break;
 				} else {
 					tag = anzahlTageMonat - anzahlTage;
 					monat = monat - 1;
 					anzahlTage = 0;
-					break;
 				}
-			case 5:
-			case 7:
-			case 10:
-			case 12:
-				anzahlTageMonat = 30;
-				if (anzahlTage >= anzahlTageMonat) {
-					anzahlTage = anzahlTage - anzahlTageMonat;
-					monat = monat - 1;
-					break;
-				} else {
-					tag = anzahlTageMonat - anzahlTage;
-					monat = monat - 1;
-					anzahlTage = 0;
-					break;
-				}
-
-			case 3:
-				if (pruefeSchaltjahr(jahr)) {
-					anzahlTageMonat = 29;
-					if (anzahlTage >= anzahlTageMonat) {
-						anzahlTage = anzahlTage - anzahlTageMonat;
-						monat = monat - 1;
-						break;
-					} else {
-						tag = anzahlTageMonat - anzahlTage;
-						monat = monat - 1;
-						anzahlTage = 0;
-						break;
-					}
-
-				} else {
-					anzahlTageMonat = 28;
-					if (anzahlTage >= anzahlTageMonat) {
-						anzahlTage = anzahlTage - anzahlTageMonat;
-						monat = monat - 1;
-						break;
-					} else {
-						tag = anzahlTageMonat - anzahlTage;
-						monat = monat - 1;
-						anzahlTage = 0;
-						break;
-					}
-				}
-
-			default:
+			} else {
+				// muss hardcodiert sein, da monat - 1 != 12, was benötigt wird
 				anzahlTageMonat = 31;
 				if (anzahlTage >= anzahlTageMonat) {
 					anzahlTage = anzahlTage - anzahlTageMonat;
 					monat = 12;
 					jahr = jahr - 1;
-					break;
 				} else {
 					tag = anzahlTageMonat - anzahlTage;
 					monat = 12;
 					jahr = jahr - 1;
 					anzahlTage = 0;
-					break;
 				}
-
 			}
-
 		}
-
 		return formatiereDatum(tag, monat, jahr);
-
 	}
 
 	public static void ausgebenAusgangsdatum(String wochentag, String datum, int bearbeitungszeit) {
@@ -325,6 +188,33 @@ public class Termine {
 			System.out.println("Vorwaertsterminierung ergibt als Abgabedatum: " + wochentag + ", " + datum);
 		} else {
 			System.out.println("Rueckwaertsterminierung ergibt als Anfangsdatum: " + wochentag + ", " + datum);
+		}
+	}
+	
+	public static void main(String[] args) {
+		final int TAG = 22;
+		final int MONAT = 4;
+		final int JAHR = 2019;
+		final int BEARBEITUNGSZEIT = 8;
+		final int BEARBEITUNGSZEIT_TAGE = BEARBEITUNGSZEIT * 7;
+
+		String wochentag = berechneWochentag(TAG, MONAT, JAHR);
+		String datumAusgang = formatiereDatum(TAG, MONAT, JAHR);
+		ausgebenAusgangsdatum(wochentag, datumAusgang, BEARBEITUNGSZEIT);
+
+		if (pruefeDatum(TAG, MONAT, JAHR)) {
+			if (!wochentag.equals("Samstag") && !wochentag.equals("Sonntag")) {
+
+				String datumv = berechneVorwaertsDatum(TAG, MONAT, JAHR, BEARBEITUNGSZEIT_TAGE);
+				String datumr = berechnerueckwaertsDatum(TAG, MONAT, JAHR, BEARBEITUNGSZEIT_TAGE);
+
+				ausgebenTerminierung(wochentag, datumv, "vorwaerts");
+				ausgebenTerminierung(wochentag, datumr, "rueckwaerts");
+			} else {
+				System.out.println("Ausgangsdatum ist kein Wochentag");
+			}
+		} else {
+			System.out.println("Geben Sie ein gueltiges Datum an");
 		}
 	}
 
